b0d9a7d491c6ec944e3b0e9d4480db60405acbb1
pofay <gianCarlo.gilos@outlook.com>
2019-07-01 14:14:00 +0800

    Setup redux in project

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Story from '../components/index/story';
 import Head from '../components/common/head';
 import Nav from '../components/common/nav';
 import loadDB from '../firebase-loader';
+import { connect } from 'react-redux';
 
-const Home = ({ stories }) => (
-  <div>
-    <Head title="Home" />
-    <Nav />
+class Home extends Component {
+  static async getInitialProps({ store, isServer, pathname, query }) {
+    const db = await loadDB();
 
-    <div className="hero">
-      <h1 className="title">Hackernews SSR</h1>
-    </div>
+    console.log(store.getState())
 
-    {stories.map(story => (
-      <Story
-        key={story.id}
-        url={story.url}
-        title={story.title}
-        user={story.by}
-        {...story}
-      />
-    ))}
+    const ids = await db.child('topstories').once('value');
+    const stories = await Promise.all(
+      ids
+        .val()
+        .slice(0, 20)
+        .map(id =>
+          db
+            .child('item')
+            .child(id)
+            .once('value')
+        )
+    ).then(s => s.map(s => s.val()));
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin-bottom: 40px;
-        width: 100%;
-        padding-top: 40px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-    `}</style>
-  </div>
-);
+    return { stories };
+  }
 
-Home.getInitialProps = async function() {
-  const db = await loadDB();
+  render() {
 
-  const ids = await db.child('topstories').once('value');
-  const stories = await Promise.all(
-    ids
-      .val()
-      .slice(0, 20)
-      .map(id =>
-        db
-          .child('item')
-          .child(id)
-          .once('value')
-      )
-  ).then(s => s.map(s => s.val()));
+    const { stories } = this.props
+    return (
+      <div>
+        <Head title="Home" />
+        <Nav />
 
-  return { stories };
-};
+        <div className="hero">
+          <h1 className="title">Hackernews SSR</h1>
+        </div>
 
-export default Home;
+        {stories.map(story => (
+          <Story
+            key={story.id}
+            url={story.url}
+            title={story.title}
+            user={story.by}
+            {...story}
+          />
+        ))}
+
+        <style jsx>{`
+          .hero {
+            width: 100%;
+            color: #333;
+          }
+          .title {
+            margin-bottom: 40px;
+            width: 100%;
+            padding-top: 40px;
+            line-height: 1.15;
+            font-size: 48px;
+          }
+          .title,
+          .description {
+            text-align: center;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default connect(state => state)(Home);
