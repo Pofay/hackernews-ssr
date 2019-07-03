a75bab4058d1b2bfffe0b01ca5f7b17391b4579e
pofay <gianCarlo.gilos@outlook.com>
2019-07-03 21:05:07 +0800

    Adding an individual story in redux store

--- __tests__/storiesReducerTest.js
+++ __tests__/storiesReducerTest.js
@@ -1,7 +1,60 @@
-import storiesReducer from '../components/index/reducers/storiesReducer'
+import storiesReducer from '../components/index/reducers/storiesReducer';
+import { createStore, combineReducers } from 'redux';
 
-it('Should work', () => {
+const reducerUnderTest = combineReducers({
+  stories: storiesReducer
+});
 
-  expect(1).toBe(1)
+it('Returns base state when no action type matches', () => {
+  const expected = {
+    stories: {
+      byId: {},
+      allIds: []
+    }
+  };
+  const store = createStore(reducerUnderTest, {});
 
-})
+  store.dispatch({ type: 'SOME_ACTION_TYPE' });
+  const actual = store.getState();
+
+  expect(actual).toEqual(expected);
+});
+
+it('Adding a Story returns normalized form', () => {
+  const apiResponse = {
+    by: 'dhouston',
+    descendants: 71,
+    id: 8863,
+    kids: [8980, 8934, 8876],
+    score: 111,
+    time: 1175714200,
+    title: 'My YC app: Dropbox - Throw away your USB drive',
+    type: 'story',
+    url: 'http://www.getdropbox.com/u/2/screencast.html'
+  };
+
+  const expected = {
+    stories: {
+      byId: {
+        '8863': {
+          by: 'dhouston',
+          descendants: 71,
+          id: 8863,
+          kids: [8980, 8934, 8876],
+          score: 111,
+          time: 1175714200,
+          title: 'My YC app: Dropbox - Throw away your USB drive',
+          type: 'story',
+          url: 'http://www.getdropbox.com/u/2/screencast.html'
+        }
+      },
+      allIds: [8863]
+    }
+  };
+  const store = createStore(reducerUnderTest, {});
+
+  store.dispatch({ type: 'ADD_STORY', payload: apiResponse });
+  const actual = store.getState();
+
+  expect(actual).toEqual(expected);
+});
--- components/index/reducers/storiesReducer.js
+++ components/index/reducers/storiesReducer.js
@@ -1,6 +1,36 @@
 import { normalize, schema } from 'normalizr';
-import { merge } from 'ramda';
+import { combineReducers, bindActionCreators } from 'redux';
+import { merge, head } from 'ramda';
 
 const storySchema = new schema.Entity('stories');
 const storyListSchema = [storySchema];
 
+const storyById = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_STORY':
+      const normalizedData = normalize(action.payload, storySchema);
+      const { stories } = normalizedData.entities;
+      const id = normalizedData.result
+      return {...state, [id] : stories[id] }
+    default:
+      return state;
+  }
+};
+
+const allStories = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_STORY':
+      const normalizedData = normalize(action.payload, storySchema);
+      const { result } = normalizedData;
+      return state.concat(result);
+    default:
+      return state;
+  }
+};
+
+const baseReducer = combineReducers({
+  byId: storyById,
+  allIds: allStories
+});
+
+export default baseReducer;
