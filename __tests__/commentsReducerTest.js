34e1b3985d47cde816be0d224f444b5ddb289e98
pofay <gianCarlo.gilos@outlook.com>
2019-07-05 14:58:26 +0800

    Can now add comments into redux store

--- __tests__/commentsReducerTest.js
+++ __tests__/commentsReducerTest.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux';
-import commentsReducer from '../components/comments/reducer'
+import commentsReducer from '../components/comments/reducer';
 
 const reducerUnderTest = combineReducers({
   comments: commentsReducer
@@ -20,3 +20,40 @@ it('Returns base state when no action type matches', () => {
   expect(actual).toEqual(expected);
 });
 
+it('Adding a comment into tree', () => {
+  const apiResponse = {
+    by: 'norvig',
+    id: 2921983,
+    kids: [2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141],
+    parent: 2921506,
+    text:
+      "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
+    time: 1314211127,
+    type: 'comment'
+  };
+
+  const expected = {
+    comments: {
+      byId: {
+        '2921983': {
+          by: 'norvig',
+          id: 2921983,
+          kids: [2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141],
+          parent: 2921506,
+          text:
+            "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
+          time: 1314211127,
+          type: 'comment'
+        }
+      },
+      allIds: [2921983]
+    }
+  };
+
+  const store = createStore(reducerUnderTest, {});
+
+  store.dispatch({ type: 'ADD_COMMENT', payload: apiResponse });
+  const actual = store.getState();
+
+  expect(actual).toEqual(expected);
+});
--- components/comments/reducer/index.js
+++ components/comments/reducer/index.js
@@ -1,7 +1,27 @@
 import { combineReducers } from 'redux';
+import { merge } from 'ramda';
+import { normalize, schema } from 'normalizr';
+
+const commentSchema = new schema.Entity('comments');
 
 const commentsById = (state = {}, action) => {
   switch (action.type) {
+    case 'ADD_COMMENT':
+      const normalizedData = normalize(action.payload, commentSchema);
+      const { comments } = normalizedData.entities;
+      const { result } = normalizedData;
+      return { ...state, [result]: comments[result] };
+    default:
+      return state;
+  }
+};
+
+const allComments = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      const normalizedData = normalize(action.payload, commentSchema);
+      const { result } = normalizedData;
+      return state.concat(result);
     default:
       return state;
   }
